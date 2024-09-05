c2deeedb1982e215223c8cd474a74a2d36a59c84
yujit2003 <yujityadav99@gmail.com>
2024-09-05 13:53:13 +0530

    image bug fixed

--- components/Home/Dev.jsx
+++ components/Home/Dev.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Dev = () => {
   return (
     <div className='max-h-6 w-full bg-gray-300 text-sm flex justify-center items-center text-black/50'>
-      Pitama Foods © 2024 | Website by - Gautam and <a href='https://yujit-portfolio.vercel.app/'> &nbsp;Yujit</a>
+      Pitama Foods © 2024 | Website by - &nbsp;<u>Gautam</u>&nbsp; and &nbsp;<u><a href='https://yujit-portfolio.vercel.app/'> Yujit</a></u>
     </div>
   )
 }
