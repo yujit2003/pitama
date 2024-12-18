3ad2d8695ce1c2e07ff938b3f875931f6c8515f1
Yujit Yadav <97657345+yujit2003@users.noreply.github.com>
2024-12-19 00:06:53 +0530

    Update Dev.jsx

--- components/Home/Dev.jsx
+++ components/Home/Dev.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Dev = () => {
   return (
     <div className='max-h-6 w-full bg-gray-300 text-sm flex justify-center items-center text-black/50'>
-      Pitama Foods © 2024 | Website by - &nbsp;<u><a href='https://yujit-portfolio.vercel.app/'> Yujit</a></u>&nbsp; and &nbsp<u>Gautam</u>;
+      Pitama Foods © 2024 | Website by - &nbsp;<u><a href='https://yujit-portfolio.vercel.app/'> Yujit</a></u>&nbsp; and &nbsp;<u>Gautam</u>;
     </div>
   )
 }
